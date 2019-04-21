9788ce45c8584e205945905f2632532e5701b3d1
bsheppard8 <bsheppard8@gatech.edu>
2019-04-21 16:27:22 -0400

    Deleted unneeded code

--- app/src/main/java/com/team8303/smartbox/passcode/PasscodeFragment.java
+++ app/src/main/java/com/team8303/smartbox/passcode/PasscodeFragment.java
@@ -59,22 +59,18 @@ public class PasscodeFragment extends Fragment {
                 String title = tab.getText().toString();
                 if (title.equals(_permanent)) {
                     currentTab = "Permanent";
-                    Log.d("Tag!","The current tab has changed to " + currentTab);
                   //  Toast.makeText(getContext(), _permanent, Toast.LENGTH_LONG).show();
                     adapter.setPasscodeList(Model.getPermanentPasscodes());
                 } else if (title.equals(_temp)) {
                     currentTab = "Temporary";
-                    Log.d("Tag!","The current tab has changed to " + currentTab);
                   //  Toast.makeText(getContext(), _temp, Toast.LENGTH_LONG).show();
                     adapter.setPasscodeList(Model.getTempPasscodes());
                 } else if (title.equals(_repeat)) {
                     currentTab = "Repeat";
-                    Log.d("Tag!","The current tab has changed to " + currentTab);
                   //  Toast.makeText(getContext(), _repeat, Toast.LENGTH_LONG).show();
                     adapter.setPasscodeList(Model.getRepeatPasscodes());
                 } else if (title.equals(_one_time)) {
                     currentTab = "One-Time";
-                    Log.d("Tag!","The current tab has changed to " + currentTab);
                    // Toast.makeText(getContext(), _one_time, Toast.LENGTH_LONG).show();
                     adapter.setPasscodeList(Model.getOnePasscodes());
                 }
@@ -99,7 +95,6 @@ public class PasscodeFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
-        Log.d("Tag!","The current tab is " + currentTab);
         List<Passcode> inputList = Model.getPermanentPasscodes();
         if (currentTab.equals("Temporary")) {
             inputList = Model.getTempPasscodes();
